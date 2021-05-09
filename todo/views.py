8ba9959652cbe3564a9b5748ce2a5fb9d4fecf17
yuta0709 <yf20030709@gmail.com>
2021-05-09 21:36:27 +0900

    Add processing for POST

--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from . import forms
+from . import models
 
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    if request.method == 'POST':
+        form = forms.TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+    data = {
+        'form': forms.TaskForm(),
+        'tasks': models.Task.objects.all()
+    }
+    return render(request, 'index.html', data)
